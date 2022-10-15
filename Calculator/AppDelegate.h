5c08e59cdc39a47ecd42a540a1ccf2d1d27dcf0b
Andre Rodrigues <andrecomerciale@gmail.com>
2022-10-14 22:04:06 -0300

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by André Almeida on 2022-10-14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Calculator/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Calculator
+//
+//  Created by André Almeida on 2022-10-14.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
